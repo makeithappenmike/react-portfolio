022fbda1c56a2d7fd8b873afa699c382ea1985ab
MakeithappenMike <omichaelaaron@gmail.com>
2022-11-20 12:09:40 -0500

    more setup

--- React Portfolio - Michael Martens/src/App.jsx
+++ React Portfolio - Michael Martens/src/App.jsx
@@ -15,7 +15,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hi, I'm Michael.</h1>
+      <p>A Software Engineer.</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
