14182d1d7c68b11b217ad48e1ead88e4e91e1199
MakeithappenMike <omichaelaaron@gmail.com>
2022-11-20 15:08:53 -0500

    working with useRef to get scrolling going

--- React Portfolio - Michael Martens/src/components/Navigation.jsx
+++ React Portfolio - Michael Martens/src/components/Navigation.jsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
+import {useRef} from 'react';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function Navigation({ currentPage, handlePageChange }) {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home"><img src='./../assets/mm_logo.png' /></Navbar.Brand>
+        <Navbar.Brand href="#home"><img src={'../assets/mm_logo.png'} /></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Home</Nav.Link>
-            <Nav.Link href="#pricing">About</Nav.Link>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
             <NavDropdown title="Portfolio" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#project/one">
                 Project One
@@ -30,6 +27,7 @@ function Navigation({ currentPage, handlePageChange }) {
                 Project Three
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
