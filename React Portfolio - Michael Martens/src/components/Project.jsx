6ad4a3bab31d44255a3e6f00ef398a4c6f54b394
MakeithappenMike <omichaelaaron@gmail.com>
2022-11-21 21:12:17 -0500

    project card layout centered and responsive:

--- React Portfolio - Michael Martens/src/components/Project.jsx
+++ React Portfolio - Michael Martens/src/components/Project.jsx
@@ -1,16 +1,31 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
 import 'bootstrap/dist/css/bootstrap.css';
 import placeholder from '../assets/placeholder.png';
+
+const styles = {
+  project: {
+    width: '18rem',
+    margin: '10px'
+  },
+  image: {
+    width: '100%',
+    marginTop: '10px'
+  }
+};
 
 function BasicExample() {
   return (
     <>
-      <section id='portfolio'>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={placeholder} />
+      <section class='portfolio' id='portfolio'>
+
+      {/* Project One */}
+      <Row xs={1} md={2} className="g-4 justify-content-center">
+      <Card style={styles.project} className="project">
+        <Card.Img variant="top" src={placeholder} style={styles.image} />
         <Card.Body bg='secondary'>
-          <Card.Title bg='primary' as='h4'>Card Title</Card.Title>
+          <Card.Title bg='primary' as='h4'>Project One</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
@@ -18,10 +33,12 @@ function BasicExample() {
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={placeholder} />
+
+      {/* Project Two */}
+      <Card style={styles.project} className="project">
+      <Card.Img variant="top" src={placeholder} style={styles.image} />
       <Card.Body bg='secondary'>
-        <Card.Title bg='primary' as='h4'>Card Title</Card.Title>
+        <Card.Title bg='primary' as='h4'>Project Two</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
@@ -29,10 +46,12 @@ function BasicExample() {
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
-    <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src={placeholder} />
+
+    {/* Project Three */}
+    <Card style={styles.project} className="project">
+    <Card.Img variant="top" src={placeholder} style={styles.image} />
     <Card.Body bg='secondary'>
-      <Card.Title bg='primary' as='h4'>Card Title</Card.Title>
+      <Card.Title bg='primary' as='h4'>Project Three</Card.Title>
       <Card.Text>
         Some quick example text to build on the card title and make up the
         bulk of the card's content.
@@ -40,6 +59,21 @@ function BasicExample() {
       <Button variant="primary">Go somewhere</Button>
     </Card.Body>
   </Card>
+
+  {/* Project Four */}
+  <Card style={styles.project} className="project">
+    <Card.Img variant="top" src={placeholder} style={styles.image} />
+    <Card.Body bg='secondary'>
+      <Card.Title bg='primary' as='h4'>Project Four</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card>
+  </Row>
+
   </section>
 </>
   );
